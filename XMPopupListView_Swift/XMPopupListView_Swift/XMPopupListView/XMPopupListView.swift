2c3bfca8beb9d6390e52508d90c2a2746e6bd4c9
Mazy <twt_ios@163.com>
2017-01-10 17:32:08 +0800

    创建XMPopupListView工具类

---
+++ XMPopupListView_Swift/XMPopupListView_Swift/XMPopupListView/XMPopupListView.swift
@@ -0,0 +1,21 @@
+//
+//  XMPopupListView.swift
+//  XMPopupListView_Swift
+//
+//  Created by TwtMac on 17/1/10.
+//  Copyright © 2017年 Mazy. All rights reserved.
+//
+
+import UIKit
+
+class XMPopupListView: UIControl {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
