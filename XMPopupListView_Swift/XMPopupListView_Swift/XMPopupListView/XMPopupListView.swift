47d135640a98ef28d8bf13d6b7ba7ebc69ff9d48
Mazy <twt_ios@163.com>
2017-01-11 17:08:59 +0800

    完善构造方法,添加代理

--- XMPopupListView_Swift/XMPopupListView_Swift/XMPopupListView/XMPopupListView.swift
+++ XMPopupListView_Swift/XMPopupListView_Swift/XMPopupListView/XMPopupListView.swift
@@ -9,22 +9,83 @@
 import UIKit
 
 class XMPopupListView: UIControl {
-    
-    var boundView1: UIView?
-    var dataSource1: Any?
-    var delegate1: Any?
 
-    init(boundView: UIView,datasource:Any,delegate:Any) {
+    var boundView: UIView?
+    var xm_dataSource: XMPopupListViewDataSource?
+    var xm_delegate: XMPopupListViewDelegate?
+    
+
+    init(boundView: UIView,dataSource:XMPopupListViewDataSource,delegate:XMPopupListViewDelegate) {
         
-        super.init(frame: CGRect.zero)
+        let screenBounds = UIScreen.main.bounds
+        let  fm = CGRect(x: 0, y: 0, width: screenBounds.width, height: screenBounds.height)
         
-        boundView1 = boundView
-        dataSource1 = datasource
-        delegate1 = delegate
+        super.init(frame: fm)
+        
+        backgroundColor = UIColor.init(red: 0.16, green: 0.17, blue: 0.21, alpha: 0.3)
+        
+        frame = fm
+        
+        xm_dataSource = dataSource
+        xm_delegate = delegate
+        
+        self.tableView = UITableView(frame: CGRect.zero, style: .plain)
+        self.tableView?.delegate = self
+        self.tableView?.dataSource = self
+        self.tableView?.clipsToBounds = true
+        self.tableView?.layer.cornerRadius = 3
+        self.tableView?.separatorStyle = .none
+        self.tableView?.backgroundColor = UIColor.white
+        
+        self.boundView = boundView
+       
+        self.addTarget(self, action: #selector(dismiss), for: .touchUpInside)
+        
+        isShowing = false
         
     }
+    
+    @objc func dismiss() {
+        
+    }
+    
+    func show(){
+        
+    }
+    
+    var tableView: UITableView?
+    
+    var isShowing: Bool?
+    
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
+
+protocol XMPopupListViewDelegate {
+    
+    func clickedListViewAtIndexPath(indexPath: IndexPath)
+}
+
+protocol XMPopupListViewDataSource {
+    
+    func itemCell(indexPath: IndexPath) -> UITableViewCell
+    func numberOfRowsInSection(section: NSInteger) -> NSInteger
+    
+    func numberOfSections() -> NSInteger
+    func itemCellHeight(indexPath: IndexPath) -> Float
+    func titleInSection(section: NSInteger) -> String
+}
+
+
+extension XMPopupListView: UITableViewDataSource,UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
